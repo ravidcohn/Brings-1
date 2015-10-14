27fd16e23dff27b00e834ea6de56dc6a79ce7901
pinhas <pinhasn@gmail.com>
2015-10-14 21:30:54 +0300

    commit

--- backend/src/main/java/com/example/some_lie/backend/utils/MySQL_Util.java
+++ backend/src/main/java/com/example/some_lie/backend/utils/MySQL_Util.java
@@ -19,6 +19,8 @@ public class MySQL_Util {
     }
 
     public static void update(String table_name, String[] set_columns, String[] set_values, String[] where_columns, String[] where_values) throws Exception {
+        clean(set_values);
+        clean(where_values);
         String query = "update `" + table_name + "` set ";
         int end = set_columns.length - 1;
         for (int i = 0; i < end; i++) {
@@ -37,6 +39,7 @@ public class MySQL_Util {
     }
 
     public static void insert(String table_name, String[] values) throws Exception {
+        clean(values);
         String query = "insert into `" + table_name + "` values('";
         int end = values.length - 1;
         for (int i = 0; i < end; i++) {
@@ -49,6 +52,7 @@ public class MySQL_Util {
     }
 
     public static void delete(String table, String[] where_columns, String[] where_values, int[] limit)throws Exception {
+        clean(where_values);
         int end = where_columns.length - 1;
         String query = "delete from `" + table + "` where ";
         for (int i = 0; i < end; i++) {
@@ -79,6 +83,7 @@ public class MySQL_Util {
      * @return
      */
     public static ResultSet select(String[] what, String table, String[] where_columns, String[] where_values, int[] limit) throws Exception {
+        clean(where_values);
         String query = "select ";
         if (what == null) {
             query += "* ";
@@ -111,5 +116,11 @@ public class MySQL_Util {
         return conn.createStatement().executeQuery(query);
     }
 
-
+    private static void clean(String[] values) {
+        if(values != null)
+        for (int i = 0; i < values.length; i++) {
+            if(values[i] != null)
+            values[i] = values[i].replaceAll("\'","\'\'");
+        }
+    }
 }
