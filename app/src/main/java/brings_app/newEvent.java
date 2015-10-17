dd3f0dcbe388360efded4d712d75b048cb82bb74
pinhas <pinhasn@gmail.com>
2015-10-17 23:39:49 +0300

    Merge branch 'master' of https://github.com/pinhasnis/Brings
    Conflicts:
    	app/build.gradle

--- app/src/main/java/brings_app/newEvent.java
+++ app/src/main/java/brings_app/newEvent.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.StringTokenizer;
 
 import server.EventFriend_AsyncTask_insert;
 import server.Event_AsyncTask_insert;
@@ -47,49 +48,63 @@ public class newEvent extends AppCompatActivity {
     private String imagePath = "";
     private String Update_Time;
     private String USERNAME = Constants.User_Name;//TODO
+    private String location ="";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_event);
-        tv_ne_name_ui = (EditText)findViewById(R.id.tv_ne_name_ui);
-        tv_ne_place_ui = (EditText)findViewById(R.id.tv_ne_place_ui);
-        tv_ne_start_ui = (EditText)findViewById(R.id.tv_ne_start_ui);
-        tv_ne_end_ui = (EditText)findViewById(R.id.tv_ne_end_ui);
-        tv_ne_description_ui = (EditText)findViewById(R.id.tv_ne_description_ui);
-        iv_ne_pic_ui = (ImageView)findViewById(R.id.iv_ne_pic_ui);
-        bt_ne_pic_ui = (Button)findViewById(R.id.bt_ne_pic_ui);
-        bt_ne_create_event_ui = (Button)findViewById(R.id.bt_ne_create_event_ui);
+        tv_ne_name_ui = (EditText) findViewById(R.id.tv_ne_name_ui);
+        tv_ne_place_ui = (EditText) findViewById(R.id.tv_ne_place_ui);
+        tv_ne_start_ui = (EditText) findViewById(R.id.tv_ne_start_ui);
+        tv_ne_end_ui = (EditText) findViewById(R.id.tv_ne_end_ui);
+        tv_ne_description_ui = (EditText) findViewById(R.id.tv_ne_description_ui);
+        iv_ne_pic_ui = (ImageView) findViewById(R.id.iv_ne_pic_ui);
+        bt_ne_pic_ui = (Button) findViewById(R.id.bt_ne_pic_ui);
+        bt_ne_create_event_ui = (Button) findViewById(R.id.bt_ne_create_event_ui);
+
+        // String url = "waze://?q=";
+        // final Intent waze = new Intent( Intent.ACTION_VIEW, Uri.parse( url ) );
+        //           startActivity( waze );
+
+        final Intent maps = new Intent(this, google_map_location.class);
+        tv_ne_place_ui.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(maps, 26);
+            }
+        });
+
 
         //final Intent tabs = new Intent(this,tab.class);
-        final Intent tabs2 = new Intent(this,tab.class);
+        final Intent tabs2 = new Intent(this, tab.class);
         final Context context = this;
         bt_ne_create_event_ui.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 boolean ok = saveData();
-                if(ok) {
-                    String key = USERNAME+" - "+ID;
+                if (ok) {
+                    String key = USERNAME + " - " + ID;
                     String name = tv_ne_name_ui.getText().toString();
-                    String place = tv_ne_place_ui.getText().toString();
+                    String place = location;
                     String start = tv_ne_start_ui.getText().toString();
                     String end = tv_ne_end_ui.getText().toString();
                     String description = tv_ne_description_ui.getText().toString();
                     new Event_AsyncTask_insert(context).execute(key, name, place, start, end, description, imagePath, Update_Time);
-                    new EventFriend_AsyncTask_insert(context).execute(key,USERNAME,Constants.Yes);
+                    new EventFriend_AsyncTask_insert(context).execute(key, USERNAME, Constants.Yes);
                     Bundle b = new Bundle();
                     b.putString("KEY", key);
-                 //   b.putString("USERNAME",USERNAME);
+                    //   b.putString("USERNAME",USERNAME);
                     tabs2.putExtras(b);
-                        // startActivity(tabs);
-                        startActivity(tabs2);
+                    // startActivity(tabs);
+                    startActivity(tabs2);
                     finish();
-                    }
-                   else{
-                        Toast.makeText(getApplicationContext(),"only description can by empty..",Toast.LENGTH_SHORT).show();
-                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "only description can by empty..", Toast.LENGTH_SHORT).show();
+                }
             }
 
         });
@@ -97,20 +112,20 @@ public class newEvent extends AppCompatActivity {
         setOnClick();
     }
 
-    private boolean saveData(){
+    private boolean saveData() {
         boolean ok = false;
-        if(tv_ne_name_ui.getText().length() > 0 && tv_ne_place_ui.length() > 0 && tv_ne_start_ui.length() > 0 && tv_ne_end_ui.length() > 0) {
+        if (tv_ne_name_ui.getText().length() > 0 && tv_ne_place_ui.length() > 0 && tv_ne_start_ui.length() > 0 && tv_ne_end_ui.length() > 0) {
             ok = true;
             int id = 0;
-            String key = USERNAME + " - "+id;
+            String key = USERNAME + " - " + id;
             ArrayList<String> allIDS = new ArrayList<>();
-            ArrayList<String>[] dbResult = sqlHelper.select(null,Constants.Table_Events,null,null,null);
-            for (String t_id : dbResult[0]){
+            ArrayList<String>[] dbResult = sqlHelper.select(null, Constants.Table_Events, null, null, null);
+            for (String t_id : dbResult[0]) {
                 allIDS.add(t_id);
             }
-            while (allIDS.contains(key)){
+            while (allIDS.contains(key)) {
                 id++;
-                key = USERNAME + " - "+id;
+                key = USERNAME + " - " + id;
             }
             ID = id;
             String name = tv_ne_name_ui.getText().toString();
@@ -120,8 +135,8 @@ public class newEvent extends AppCompatActivity {
             String description = tv_ne_description_ui.getText().toString();
             Date time = Calendar.getInstance().getTime();
             Update_Time = time.toString();
-            sqlHelper.insert(Constants.Table_Events,new String[]{key,name,place,start,end,description,imagePath,Update_Time});
-            sqlHelper.insert(Constants.Table_Events_Friends, new String[]{key,Constants.User_nickName,Constants.Yes});
+            sqlHelper.insert(Constants.Table_Events, new String[]{key, name, place, start, end, description, imagePath, Update_Time});
+            sqlHelper.insert(Constants.Table_Events_Friends, new String[]{key, Constants.User_nickName, Constants.Yes});
         }
         return ok;
     }
@@ -199,15 +214,17 @@ public class newEvent extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-        private void updateLabelStart(Calendar myCalendar) {
 
-            String myFormat = "dd/MM/yy"; //In which you need put here
-            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+    private void updateLabelStart(Calendar myCalendar) {
 
-            tv_ne_start_ui.setText(sdf.format(myCalendar.getTime()));
-            tv_ne_end_ui.setText(sdf.format(myCalendar.getTime()));
-            tv_ne_description_ui.requestFocus();
-        }
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        tv_ne_start_ui.setText(sdf.format(myCalendar.getTime()));
+        tv_ne_end_ui.setText(sdf.format(myCalendar.getTime()));
+        tv_ne_description_ui.requestFocus();
+    }
+
     private void updateLabelEnd(Calendar myCalendar) {
 
         String myFormat = "dd/MM/yy"; //In which you need put here
@@ -218,8 +235,8 @@ public class newEvent extends AppCompatActivity {
     }
 
 
-    private void setOnClick(){
-        final Intent new_event = new Intent(this,newEvent.class);
+    private void setOnClick() {
+        final Intent new_event = new Intent(this, newEvent.class);
 
         bt_ne_pic_ui.setOnClickListener(new View.OnClickListener() {
 
@@ -240,19 +257,27 @@ public class newEvent extends AppCompatActivity {
             if (requestCode == 1) {
 
                 Uri selectedImage = data.getData();
-                String[] filePath = { MediaStore.Images.Media.DATA };
+                String[] filePath = {MediaStore.Images.Media.DATA};
                 Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
                 c.moveToFirst();
                 int columnIndex = c.getColumnIndex(filePath[0]);
                 String picturePath = c.getString(columnIndex);
                 c.close();
                 imagePath = picturePath;
-                Bitmap thumbnail =  bitmapHelper.decodeSampledBitmapFromFile(picturePath, 100, 100);//(BitmapFactory.decodeFile(picturePath));
+                Bitmap thumbnail = bitmapHelper.decodeSampledBitmapFromFile(picturePath, 100, 100);//(BitmapFactory.decodeFile(picturePath));
                 iv_ne_pic_ui.setImageBitmap(thumbnail);
+            }
+            else if(requestCode == 26){
+                Bundle b = data.getExtras();
+                location = b.getString("location");
+                StringTokenizer tok = new StringTokenizer("$&$");
+                tok.nextToken();
+                String locationTag = tok.nextToken();
+                tv_ne_place_ui.setText(locationTag);
+
             }
         }
     }
-
 
 
 }
