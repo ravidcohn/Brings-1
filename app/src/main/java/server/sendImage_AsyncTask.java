04a142b2a329046a4d0113c8f8903eda9b19dc81
pinhas <pinhasn@gmail.com>
2015-10-21 15:13:07 +0300

    commit

--- app/src/main/java/server/sendImage_AsyncTask.java
+++ app/src/main/java/server/sendImage_AsyncTask.java
@@ -3,41 +3,41 @@ package server;
 /**
  * Created by pinhas on 19/10/2015.
  */
+
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+
 import android.os.AsyncTask;
+
+import com.google.api.client.http.HttpResponse;
 
 import utils.Constants;
 
 public class sendImage_AsyncTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
-        try{
-            URL url = new URL(Constants.ROOT_URL.substring(0,Constants.ROOT_URL.length()-8)+"blob_image");
-            URLConnection connection = url.openConnection();
+        try {
 
-            String inputString = params[0];//inputValue.getText().toString();
-            connection.setDoOutput(true);
-            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-            out.write(inputString);
-            out.close();
+            URL url = new URL(Constants.ROOT_URL.substring(0, Constants.ROOT_URL.length() - 8) + "blob_image");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            //urlConnection.setRequestProperty("User-Agent", USER_AGENT);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String returnString="";
-            doubledValue =0;
-
-            while ((returnString = in.readLine()) != null)
-            {
-                doubledValue= Integer.parseInt(returnString);
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                //readStream(in);
+            } catch (Exception e1) {
+            } finally {
+                urlConnection.disconnect();
             }
-            in.close();
 
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -46,3 +46,80 @@ public class sendImage_AsyncTask extends AsyncTask<String, Void, String> {
 
 
 }
+/*
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "https://selfsolve.apple.com/wcResults.do";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+
+ */
