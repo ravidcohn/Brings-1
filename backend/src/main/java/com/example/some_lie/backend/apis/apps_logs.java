a95202da04a2865ed2a62496d90390b34b5b10ec
pinhas <pinhasn@gmail.com>
2015-10-21 16:35:58 +0300

    commit

--- backend/src/main/java/com/example/some_lie/backend/apis/apps_logs.java
+++ backend/src/main/java/com/example/some_lie/backend/apis/apps_logs.java
@@ -32,7 +32,7 @@ import javax.inject.Named;
 )
 public class apps_logs {
     @ApiMethod(name = "addLog", path = "addLog")
-    public void DeleteBy_Friend(@Named("exception_string") String eString, @Named("date") String date, @Named("time") String time) {
+    public void addLog(@Named("exception_string") String eString, @Named("date") String date, @Named("time") String time) {
         try {
             MySQL_Util.insert("Logs_Apps", new String[]{eString,date,time});
         } catch (Exception e) {
