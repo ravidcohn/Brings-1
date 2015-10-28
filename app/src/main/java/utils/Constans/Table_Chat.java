310506bb347f84a3f28ba38df778e90f2cda2ac4
ravid_cohen <ravidcohn@gmail.com>
2015-10-28 23:28:56 +0200

    --

--- app/src/main/java/utils/Constans/Table_Chat.java
+++ app/src/main/java/utils/Constans/Table_Chat.java
@@ -6,8 +6,6 @@ package utils.Constans;
 public final class Table_Chat {
     public final static String Table_Name = "Chat_";//The full name is: "Chat_"+EventID".
 
-    public final static String[] Table_Chat_Fields = new String[]{"Message_ID","Friend_ID","Message","Date","Time"};
-
     public final static String Message_ID = "Message_ID";
     private final static String Message_ID_SQL_Params = "varchar NOT NULL";
     public final static String Friend_ID = "Friend_ID";
